1f0f5d770c7a939007ea4409ab2c489405284a8c
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-07-26 22:43:09 +0300

    ...

---
+++ data engineering/queries/.ipynb_checkpoints/removing-unnamed-col-checkpoint.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles`
+DROP COLUMN `Unnamed: 0`;
+
+ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.upwork_profiles`
+DROP COLUMN `Unnamed: 0`;
+
+ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.linkedin_jobs`
+DROP COLUMN `Unnamed: 0`;
