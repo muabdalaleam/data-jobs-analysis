7ebb19a04024ad1743a4811cfd4a4e98704429e1
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-07-27 11:37:52 +0300

    Removed the most of Nans with SQL

--- data engineering/queries/.ipynb_checkpoints/removing-nans-checkpoint.sql
+++ data engineering/queries/.ipynb_checkpoints/removing-nans-checkpoint.sql
@@ -1,19 +1,30 @@
-SELECT *,
-       IFNULL(reqierd_credential, 'Not Specified') AS reqierd_credential_1,
-       CASE
-          WHEN (LOWER(describtion) LIKE '%remote%' OR 
-                LOWER(describtion) LIKE '%from home%') AND
-               (location_type != 'TELECOMMUTE') THEN 'Remote'
+UPDATE `data-jobs-analysis-db.data_jobs_analysis_db.linkedin_jobs`
+SET
+  reqierd_credential = IFNULL(reqierd_credential, 'Not Specified'),
+  location_type = CASE
+                   WHEN (LOWER(describtion) LIKE '%remote%' OR 
+                         LOWER(describtion) LIKE '%from home%') AND
+                        (location_type != 'TELECOMMUTE') THEN 'Remote'
 
-          WHEN (LOWER(describtion) LIKE '%hybrid%') OR
-               (location_type = 'TELECOMMUTE') THEN 'Hybrid'
+                   WHEN (LOWER(describtion) LIKE '%hybrid%') OR
+                        (location_type = 'TELECOMMUTE') THEN 'Hybrid'
 
-          WHEN (LOWER(describtion) LIKE '%office%') OR
-               (LOWER(describtion) LIKE '%in-person%') OR 
-               (LOWER(describtion) LIKE '%in-person%') THEN 'From office'
-         ELSE 'Remote' -- We will set the Non-Specified locaion type to Remote
+                   WHEN (LOWER(describtion) LIKE '%office%') OR
+                        (LOWER(describtion) LIKE '%in-person%') THEN 'From office'
 
-       END AS location_type_1
-FROM `data-jobs-analysis-db.data_jobs_analysis_db.linkedin_jobs`
-WHERE (describtion IS NOT NULL)
-LIMIT 500;
+                   ELSE 'Remote' -- We will set the Non-Specified location type to Remote
+
+                 END
+WHERE (describtion IS NOT NULL) AND (listing_dates IS NULL);
+
+
+UPDATE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles`
+SET
+  feedback = IFNULL(feedback, '0%'),
+  earnings_amount = CASE
+                     WHEN (earnings_amount IS NULL) OR 
+                          (earnings_amount = 'ID Verified') OR
+                          (earnings_amount = 'Past Earnings') THEN '0'
+                     ELSE earnings_amount
+                   END;
+
