584a363c463573794c168c38d119179c5ee8df64
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-08-01 10:46:36 +0300

    Finished the data extraction and some cleaning

--- data engineering/queries/.ipynb_checkpoints/removing-useless-cols-checkpoint.sql
+++ data engineering/queries/.ipynb_checkpoints/removing-useless-cols-checkpoint.sql
@@ -2,11 +2,13 @@
 
 ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles`
 DROP COLUMN `feedback`,
+DROP COLUMN `new_columns`,
 DROP COLUMN `earnings_amount`,
 DROP COLUMN `hour_rate`,
 DROP COLUMN `Unnamed: 0`;
 
 ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.upwork_profiles`
+DROP COLUMN `new_columns`,
 DROP COLUMN `earnings_amount`,
 DROP COLUMN `hour_rate`,
 DROP COLUMN `Unnamed: 0`;
