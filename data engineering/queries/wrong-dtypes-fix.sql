99120b7f1a1689a34f3fe2e55ea8314f524c9b07
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-08-05 17:59:01 +0300

    Recreated the SQL queries

--- data engineering/queries/wrong-dtypes-fix.sql
+++ data engineering/queries/wrong-dtypes-fix.sql
@@ -6,20 +6,16 @@ SELECT *,
   STRUCT(
     -- Collecting the numric data from the text data using REGEXP
     CASE
-      WHEN earnings_amount LIKE '%K%' THEN CAST(REGEXP_REPLACE(earnings_amount, '[^0-9]', '') AS INT64) * 1000
-      WHEN earnings_amount LIKE '%M%' THEN CAST(REGEXP_REPLACE(earnings_amount, '[^0-9]', '') AS INT64) * 1000000 -- Million
+      WHEN LOWER(earnings_amount) LIKE '%k%' THEN CAST(REGEXP_REPLACE(earnings_amount, '[^0-9]', '') AS INT64) * 1000
+      WHEN LOWER(earnings_amount) LIKE '%m%' THEN CAST(REGEXP_REPLACE(earnings_amount, '[^0-9]', '') AS INT64) * 1000000
       ELSE CAST(REGEXP_REPLACE(earnings_amount, '[^0-9]', '') AS INT64)
     END                                          AS earnings_amount_new,
-      
+
     CAST(REPLACE(hour_rate, '$', '') AS FLOAT64) AS hour_rate_new
   ) AS new_columns
 
 FROM `data-jobs-analysis-db.data_jobs_analysis_db.upwork_profiles`;
 
-ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.upwork_profiles`
-  DROP COLUMN earnings_amount,
-  DROP COLUMN hour_rate;
-  
 ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.upwork_profiles_temp`
   DROP COLUMN earnings_amount,
   DROP COLUMN hour_rate;
@@ -45,18 +41,13 @@ CREATE OR REPLACE TABLE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profil
 AS
 SELECT *,
   STRUCT(
-    CAST(REGEXP_REPLACE(SUBSTRING(earnings_amount, 1, 10), '[^0-9]', '') AS INT64)   AS earnings_amount_new,
-    CAST(REGEXP_REPLACE(hour_rate,       '[^0-9]', '') AS FLOAT64) AS hour_rate_new,
-    CAST(REGEXP_REPLACE(feedback,        '[^0-9]', '') AS FLOAT64) AS feedback_new
+    CAST(REGEXP_REPLACE(earnings_amount,        '[^0-9]', '') AS INT64)   AS earnings_amount_new,
+    CAST(REGEXP_REPLACE(LEFT(hour_rate, 10),    '[^0-9]', '') AS FLOAT64) AS hour_rate_new,
+    CAST(REGEXP_REPLACE(feedback,               '[^0-9]', '') AS FLOAT64) AS feedback_new
 ) AS new_columns
 
 FROM `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles`;
 
--- Dropping the old columns from the main & temp tables
-ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles`
-  DROP COLUMN earnings_amount,
-  DROP COLUMN hour_rate,
-  DROP COLUMN feedback;
 
 ALTER TABLE `data-jobs-analysis-db.data_jobs_analysis_db.guru_profiles_temp`
   DROP COLUMN earnings_amount,
