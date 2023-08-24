47281dc4b67b0245fa4083fd4472712990e710da
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-08-24 07:54:39 +0300

    syntax error fix

--- data mining/upload-data.py
+++ data mining/upload-data.py
@@ -1,11 +1,11 @@
-''' 
+"""
 This files will take the CSV files in the data folder & upload them to BigQuerry
-Replacing the other ones so you'll need to run the cleaning querries after saving
+Replacing the other ones so you\'ll need to run the cleaning querries after saving
 the data again. 
 
 *(the cleaned data will be saved in the `cleaned-data` folder in the 
   data-engineering directory.)
-'''
+"""
 
 from google.cloud import bigquery
 import pandas as pd
