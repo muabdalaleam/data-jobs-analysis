1602d1263852132f63db13f05319b812fd681e31
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-11-20 09:17:03 +0200

    Working on the code example notebook

--- ai-app/.ipynb_checkpoints/collect-data-checkpoint.py
+++ ai-app/.ipynb_checkpoints/collect-data-checkpoint.py
@@ -14,10 +14,12 @@ import numpy as np
 
 def parse_arguments():
     
-    parser = argparse.ArgumentParser(description='Collect job data and train a ChatBot to create a Streamlit app.')
-    parser.add_argument('--country',    type=str,  required=True,   help='Specify a valid country name or a geographic region.')
-    parser.add_argument('--job_title',  type=str,  required=True,   help='Specify a job title to search for as a string.')
-    parser.add_argument('--max_pages',  type=int,  default= 1,      help='Specify the max jobs pages you want to scrape.')
+    parser = argparse.ArgumentParser(description='Collect jobs data and train a ChatBot on it and creates you a Streamlit ' + \
+                                                 'app to ask the ChatBot for jobs that fits you (FOR FREE !!!!).')
+
+    parser.add_argument('--country',    type= str,  required=True,   help= 'Specify a valid country name or a geographic region.')
+    parser.add_argument('--job_title',  type= str,  required=True,   help= 'Specify a job title to search for as a string.')
+    parser.add_argument('--max_pages',  type= int,  default= 1,      help= 'Specify the max jobs pages you want to scrape.')
     
     return parser.parse_args() 
 
@@ -27,30 +29,29 @@ def scrape_page_fast(url: str) -> BeautifulSoup:
     scraper = cloudscraper.create_scraper(delay=10, browser={'browser': 'chrome', 'platform': 'windows',
                                                              'desktop': True,     'mobile': False})
     content = scraper.get(url).text
-
     return BeautifulSoup(content, 'html.parser')
 
 
 def linkedin_scraper(country: str, job_title: str, page: int) -> BeautifulSoup:
     
-    headers = {
+    headers : dict = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
         'Accept-Encoding': '*',
-        'Connection': 'keep-alive'
-    }
+        'Connection': 'keep-alive'}
 
-    page = (page - 1) * 25
-    url = f'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={job_title}&' + \
+    page : int = (page - 1) * 25
+    url  : str = f'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={job_title}&' + \
           f'location={country}&geoId=90000084&trk=public_jobs_j%20obs-search-bar_search-submit&position=1&pageNum=0&start={page}'
     response = requests.get(url, headers=headers)
+    
     return BeautifulSoup(response.content, 'html.parser')
 
 
 def extract_job_data(job_block: BeautifulSoup) -> tuple[str]:
-    
-    title   :str = job_block.find('h3', class_='base-search-card__title').get_text(strip=True)
-    company :str = job_block.find('a', class_='hidden-nested-link').get_text(strip=True)
-    link    :str = job_block.find('a', class_='base-card__full-link', href=True)['href']
+
+    title   : str = job_block.find('h3', class_='base-search-card__title').get_text(strip=True)
+    company : str = job_block.find('a', class_='hidden-nested-link').get_text(strip=True)
+    link    : str = job_block.find('a', class_='base-card__full-link', href=True)['href']
 
     return title, company, link
 
@@ -88,7 +89,7 @@ def scrape_jobs(inputs) -> None:
         job_data.append({'job_title': title, 'company_name': company, 'job_link': link, 'desc': description})
 
     jobs_df = pd.DataFrame(job_data)
-    jobs_df.to_csv('jobs_data.csv')
+    jobs_df.to_csv('data/jobs_data.csv')
 
 
 def main():
