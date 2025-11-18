717df542731f6704a2b82255b0f2d4037875c5a8
muabdalaleam <muhammed.abdelaleam@gmail.com>
2025-11-18 04:50:05 +0200

    Turned the reports into pdf files

--- scripts/notebooks_to_reports.sh
+++ scripts/notebooks_to_reports.sh
@@ -1,2 +1,9 @@
-./venv/bin/jupyter nbconvert ./notebooks/freelancers_analysis.ipynb --output-dir='./reports'  --to 'html' 
-./venv/bin/jupyter nbconvert ./notebooks/jobs_analysis.ipynb        --output-dir='./reports'  --to 'html' 
+./venv/bin/jupyter nbconvert ./notebooks/freelancers_analysis.ipynb --no-input --output-dir='./reports'  --to 'html' 
+./venv/bin/jupyter nbconvert ./notebooks/jobs_analysis.ipynb        --no-input --output-dir='./reports'  --to 'html' 
+
+# needs you to have installed https://wkhtmltopdf.org/
+wkhtmltopdf ./reports/freelancers_analysis.html ./reports/freelancers_analysis.pdf
+wkhtmltopdf ./reports/jobs_analysis.html        ./reports/jobs_analysis.pdf
+
+rm ./reports/freelancers_analysis.html
+rm ./reports/jobs_analysis.html
